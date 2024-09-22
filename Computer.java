727b8cf19b1a09c9b88f5f30204be8863e04b667
nazzar28 <talantbekovnazar28@gmail.com>
2024-09-22 22:32:02 +0600

    png added

--- Computer.java
+++ Computer.java
@@ -1,2 +1,33 @@
 public class Computer {
+    private String brand;
+    private String model;
+    private String processor;
+    private int ram;
+
+    public Computer() {
+        this.brand = "Unknown";
+        this.model = "Unknown";
+        this.processor = "Unknown";
+        this.ram = 0;
+    }
+
+    public Computer(String brand, String model, String processor, int ram) {
+        this.brand = brand;
+        this.model = model;
+        this.processor = processor;
+        this.ram = ram;
+    }
+
+    public void displayInfo() {
+        System.out.println("Computer Info:");
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Processor: " + processor);
+        System.out.println("RAM: " + ram + " GB");
+    }
+
+    public void upgradeRam(int additionalRam) {
+        this.ram += additionalRam;
+        System.out.println("RAM updated. New RAM: " + this.ram + " GB");
+    }
 }
--- Main.java
+++ Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Computer defaultComputer = new Computer();
+        defaultComputer.displayInfo();
+
+        Computer myComputer = new Computer("Apple", "MacBook Pro", "Intel Core i7", 16);
+        myComputer.displayInfo();
+
+        myComputer.upgradeRam(8);
+    }
 }
